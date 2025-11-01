87b8e5df0135297f0219a9ebc12e3a7b59554208
Pandu1712 <prasannakumarpanda2000@gmail.com>
2025-11-01 10:32:34 +0530

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+import Navbar from "./components/Navbar";
+
 export default function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-100">
-      <h1 className="text-4xl text-blue-600 underline">
-        ✅ Tailwind CSS is Working!
-      </h1>
-    </div>
+   <>
+   <Navbar />
+   </>
   )
 }
